5dcaccb9013f9dd4f0f7dc7c71b1500a1aa6b6e7
Joanne Lee <vernapii@gmail.com>
2018-05-02 05:24:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,12 +5,14 @@ class PigLatinizer
       to_pig_latin(text)
     else
       binding.pry
-      if w[0].scan(/[aeiouAEIOU]/).count > 0
-        "#{w}way"
-      elsif w[0..1].scan(/[bcdfghjklmnpqrstvwxyz]/).count > 1
-          "#{w[2..-1]}#{w[0..1]}ay"
+      if text[0].scan(/[aeiouAEIOU]/).count > 0
+        "#{text}way"
+      elsif text[0..2].scan(/[bcdfghjklmnpqrstvwxyz]/).count >= 2
+          "#{text[3..-1]}#{text[0..2]}ay"
+      elsif text[0..1].scan(/[bcdfghjklmnpqrstvwxyz]/).count == 1
+          "#{text[2..-1]}#{text[0..1]}ay"
       else
-          "#{w[1..-1]}#{w[0]}ay"
+          "#{text[1..-1]}#{text[0]}ay"
       end
     end
   end
